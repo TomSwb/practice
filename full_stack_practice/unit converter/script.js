c2c3a9ff425f5f83316491a565f60d7c42ad48de
tomswb <thomasschwab146@gmail.com>
2025-06-30 21:04:15 +0200

    Adding unit converter exercise

---
+++ full_stack_practice/unit converter/script.js
@@ -0,0 +1,20 @@
+
+function convert(){
+    const inputValue = document.getElementById("userInput").value;
+    const unit = document.getElementById("unit").value;
+    const milesToKm = false;
+    let result = 0;
+
+    if (unit === "milesToKm"){
+      result = (inputValue * 1.60934);
+    } else {
+    result = (inputValue / 1.60934);
+    }
+
+    const resultString = inputValue + " miles are " + result + " km";
+
+    console.log(resultString);
+
+    const resultElement = document.getElementById("resultElement");
+    resultElement.innerHTML = resultString;
+}
